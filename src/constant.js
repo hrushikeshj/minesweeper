ccd293b91955f413a15b61af13c751d4bae7a0b7
hrushikesh <hrushikeshj.201cs220@nitk.edu.in>
2022-06-30 01:42:07 +0530

    add export

--- src/constant.js
+++ src/constant.js
@@ -1,10 +1,10 @@
-const row = 10, col = 10, no_mine = 10, size = 40;
+const row = 12, col = 9, no_mine = 16, size = 40;
 
 export const DEFAULT_VAL = {row: row, col: col, no_mines: no_mine}
 export const DEFAULT_CSS =  {size: size, gl: '#c3ed70',gd: '#a1d343',bl: '#f3bf8b',bd: '#d49860'}
 
 export const DEFAULT_APP_STATE  = {
     ROW: row,
-    COL: row,
+    COL: col,
     NO_MINES: no_mine
 }
